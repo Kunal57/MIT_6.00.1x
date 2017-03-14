71254adbaa6714116ed95e8398ccf1db2394cfb1
Kunal Patel <kunalpatelx92@gmail.com>
2017-03-14 15:54:12 -0500

    Eliminate say method from ArrogantProfessor class and modify say method in Professor class

--- final/problem6.py
+++ final/problem6.py
@@ -16,13 +16,11 @@ class Lecturer(Person):
 
 class Professor(Lecturer):
     def say(self, stuff):
-        return self.name + ' says: ' + self.lecture(stuff)
+        return "Prof. " + self.name + ' says: ' + self.lecture(stuff)
 
 class ArrogantProfessor(Professor):
     def lecture(self, stuff):
-        return 'It is obvious that ' + Lecturer.lecture(self, stuff)
-    def say(self, stuff):
-        return self.name + ' says: ' + self.lecture(stuff)
+        return  'It is obvious that ' + Lecturer.lecture(self, stuff)
 
 
 e = Person('eric')
@@ -33,7 +31,9 @@ ae = ArrogantProfessor('eric')
 e.say('the sky is blue')
 le.say('the sky is blue')
 le.lecture('the sky is blue')
-pe.say('the sky is blue')
+print(pe.say('the sky is blue'))
+# Prof. eric says: I believe that eric says: the sky is blue
 pe.lecture('the sky is blue')
 print(ae.say('the sky is blue'))
-print(ae.lecture('the sky is blue'))
+# Prof. eric says: It is obvious that I believe that eric says: the sky is blue
+ae.lecture('the sky is blue')
