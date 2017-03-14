351bfe236f183c069314f5df7d3c4b8f9d8699b4
Kunal Patel <kunalpatelx92@gmail.com>
2017-03-14 14:16:13 -0500

    Modify lecture method in ArrogantProfessor class using inheritance

--- final/problem6.py
+++ final/problem6.py
@@ -20,7 +20,7 @@ class Professor(Lecturer):
 
 class ArrogantProfessor(Professor):
     def lecture(self, stuff):
-        return 'It is obvious that ' + Person.say(self, stuff)
+        return 'It is obvious that ' + Lecturer.lecture(self, stuff)
     def say(self, stuff):
         return self.name + ' says: ' + self.lecture(stuff)
 
